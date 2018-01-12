eb0aaf873cc459ac5193048b37aac92612854cc2
Aswathy Pavithran <achupavi000@gmail.com>
2018-01-12 10:04:52 +0530

    Finance Tracker

---
+++ db/migrate/20180101130920_create_friendships.rb
@@ -0,0 +1,10 @@
+class CreateFriendships < ActiveRecord::Migration[5.1]
+  def change
+    create_table :friendships do |t|
+
+      t.timestamps
+      t.belongs_to :user
+      t.belongs_to :friend , class: 'User'
+    end
+  end
+end
